68563278f57ca718f803cc237d9e649ffffee002
Matthew O'Mara <omara.matthew1@gmail.com>
2020-01-22 00:07:35 -0500

    Add previous uncommited solutions

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(repeat, times) {
+
+    if (times === 0) {
+        return '';
+    } else if (times < 0) {
+        return 'ERROR';
+    }
+
+    let original = repeat;
+    for (let i = 0; i < times-1; i++) {
+        repeat = repeat.concat(original);
+
+    }
+
+    return repeat;
 
 }
 
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function (normal) {
+
+    let reverse = '';
+    for (let i = 0; i < normal.length; i++) {
+        reverse = reverse.concat(normal.charAt((normal.length - 1) - i));
+    }
+
+    return reverse;
 
 }
 
