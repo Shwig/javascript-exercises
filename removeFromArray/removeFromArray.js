abacad8991d3b88e71e3a15feb4a3874fc2a41f9
Matthew O'Mara <omara.matthew1@gmail.com>
2020-01-21 23:38:14 -0500

    Solution revision

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -4,19 +4,22 @@ const removeFromArray = function (array, ...items) {
     console.log(array);
 
     items.forEach(item => {
-        
-        console.log('first level');
-        array.forEach(element => {
 
-            console.log('second level');
-            if (element === item) {
+        if (array.includes(item)) {
+            array.splice(array.indexOf(item),1);
+        }
+        // console.log('first level');
+        // array.forEach(element => {
 
-                console.log('third level');
-                array.splice(array.indexOf(item), 1);
-            }
-        });
+        //     console.log('second level');
+        //     if (element === item) {
+
+        //         console.log('third level');
+        //         array.splice(array.indexOf(item), 1);
+        //     }
+        // });
     });
-    
+
     console.log(items);
     console.log(array);
 }
