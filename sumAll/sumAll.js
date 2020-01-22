3c9fe6e03bff75f1156c00b8e0d8f5a89d3774e7
Matthew O'Mara <omara.matthew1@gmail.com>
2020-01-22 12:21:24 -0500

    Add solution to sumall function

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+
+    if (((first < 0) || (second < 0)) ||
+        ((typeof first !== "number") || (typeof second !== "number"))) {
+
+        console.log('ERROR');
+        return 'ERROR'
+
+    }
+
+    let n = Math.abs(first - second) + 1;
+    console.log(n);
+
+    return ((n / 2) * (first + second));
 
 }
 
