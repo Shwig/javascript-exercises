5fe5ef6ec13f54bb6fda52255aa839870accade6
Matthew O'Mara <omara.matthew1@gmail.com>
2020-01-22 18:48:16 -0500

    Add solution for learyears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+
+    console.log(year)
+
+    if (((year % 100 === 0) && (year % 400 !== 0)) || (year % 4 !== 0)) {
+        console.log(year + " false")
+        return false;
+    }
+
+    console.log(year + " true")
+    return true;
 
 }
 
