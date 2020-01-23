8595cda36e1c88a3fd02c33b537e483ae8fb5997
Matthew O'Mara <omara.matthew1@gmail.com>
2020-01-22 19:10:23 -0500

    adjust format

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,7 +1,8 @@
 const sumAll = function (first, second) {
 
     if (((first < 0) || (second < 0)) ||
-        ((typeof first !== "number") || (typeof second !== "number"))) {
+        ((typeof first !== "number") || 
+        (typeof second !== "number"))) {
 
         console.log('ERROR');
         return 'ERROR'
